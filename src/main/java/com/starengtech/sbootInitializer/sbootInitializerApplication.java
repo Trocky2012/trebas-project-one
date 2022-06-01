9f85136608ea96068b239f5c73b60a19f0561004
Thiago Trolle <thiagotrolle@yahoo.com.br>
2022-06-01 18:55:50 -0400

    Project created

---
+++ src/main/java/com/starengtech/sbootInitializer/sbootInitializerApplication.java
@@ -0,0 +1,12 @@
+package com.starengtech.sbootInitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sbootInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(sbootInitializerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/starengtech/sbootInitializer/OlaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starengtech.sbootInitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
